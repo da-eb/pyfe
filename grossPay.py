238aecedee6985a6f9b957d8bad8ec4afae22179
da-eb <davidlaebube@gmail.com>
2022-01-08 00:50:50 +0100

    2

---
+++ grossPay.py
@@ -0,0 +1,11 @@
+def grossPay():
+    hours = input("Enter number of hours worked: ")
+    rate = input("Enter the agreed per hour: ")
+
+    pay = int(hours) * float(rate)
+
+    print("payment is ", pay)
+
+
+if __name__== "__main__":
+    grossPay()
